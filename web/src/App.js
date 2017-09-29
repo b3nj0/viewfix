55d59bb2984eb2ab9187b5e2b005183a6596d417
Benjamin Jenks <jenksb@gmail.com>
2017-09-29 18:25:57 +0100

    App.js: use MatchStatus for TCR summary

--- web/src/App.js
+++ web/src/App.js
@@ -47,10 +47,12 @@ class FixMsgType extends Component {
 
     if (msgtype === '8') { // execution report
       color = 'blue';
+      // fid: 150 = exec type
       text = 'ER ' + msg.tag(150).enum.replace('/_/g', ' ');
     } else if (msgtype === 'AE') { // trade capture report
       color = 'blue';
-      text = 'TCR ' + msg.tag(150).enum.replace('/_/g', ' ');
+      // fid: 573 = match status
+      text = 'TCR ' + msg.tag(573).enum.replace('/_/g', ' '); 
     } else if (msgtype === 'D') { // new order
       color = 'green';
     } else if (msgtype === '3') { // reject
